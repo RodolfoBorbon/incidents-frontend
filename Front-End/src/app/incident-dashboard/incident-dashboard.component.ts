ef88e9055b1f4b61e66c19b59273c97eec1af4e4
Rodolfo Borbon <122333787+RodolfoBorbon@users.noreply.github.com>
2023-07-21 17:45:16 -0400

    capture incident details and save it by calling the backend API.

--- Front-End/src/app/incident-dashboard/incident-dashboard.component.ts
+++ Front-End/src/app/incident-dashboard/incident-dashboard.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,8 +8,37 @@ import { Component } from '@angular/core';
 })
 export class IncidentDashboardComponent {
   incidentDescription: string ="";
+  incidentPriority: string ="";
   customerName: string ="";
   customerPhoneNumber: string ="";
   customerAddress: string ="";
   incidentNarrative: string ="";
+
+
+constructor(private http: HttpClient){ }
+
+//funtion being called when the user click the button: createIncidentTicket
+createIncidentTicket()
+{
+  var bodyData = {
+  "incidentDescription": this.incidentDescription,
+  "incidentPriority": this.incidentPriority,
+  "customerName": this.customerName,
+  "customerPhoneNumber": this.customerPhoneNumber,
+  "customerAddress": this.customerAddress,
+  "incidentNarrative": this.incidentNarrative 
+};
+
+this.http.post("http://localhost:8000/user/create",bodyData).subscribe((resultData: any) =>
+{
+  console.log(resultData);
+  alert("Incident registered successfully")
+  this.incidentDescription = '';
+  this.incidentPriority = '';
+  this.customerName = '';
+  this.customerPhoneNumber = '';
+  this.customerAddress = '';
+  this.incidentNarrative = '';
+  });
+ }
 }
