48ab386fc6abc6e5b5b4b1f4758872267a9968c1
Rodolfo Borbon <122333787+RodolfoBorbon@users.noreply.github.com>
2023-07-21 15:22:46 -0400

    Incident form created

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IncidentDashboardComponent } from './incident-dashboard/incident-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IncidentDashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ Front-End/src/app/incident-dashboard/incident-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incident-dashboard',
+  templateUrl: './incident-dashboard.component.html',
+  styleUrls: ['./incident-dashboard.component.css']
+})
+export class IncidentDashboardComponent {
+
+}
