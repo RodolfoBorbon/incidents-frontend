e0803f8e5aa622bffb054ae6fffb4d11480acdb5
Rodolfo Borbon <122333787+RodolfoBorbon@users.noreply.github.com>
2023-07-21 20:35:35 -0400

    CRUD funtionality creaded and applied

--- Front-End/src/app/incident-dashboard/incident-dashboard.component.ts
+++ Front-End/src/app/incident-dashboard/incident-dashboard.component.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './incident-dashboard.component.html',
   styleUrls: ['./incident-dashboard.component.css']
 })
-export class IncidentDashboardComponent {
+export class IncidentDashboardComponent 
+{
+  IncidentArray : any[] = [];
+
+  currentIncidentID = "";
+
   incidentDescription: string ="";
   incidentPriority: string ="";
   customerName: string ="";
@@ -15,10 +20,32 @@ export class IncidentDashboardComponent {
   incidentNarrative: string ="";
 
 
-constructor(private http: HttpClient){ }
+constructor(private http: HttpClient)
+{
+  this.getAllIncident();
+ }
+  getAllIncident() {
+    this.http.get("http://localhost:5000/incidents").subscribe((resultData: any)=>
+    {
+      console.log(resultData);
+      this.IncidentArray = resultData.data;
+    });
+  }
+
+  
+  setUpdate(data: any) 
+  {
+    this.incidentDescription = data.incidentDescription;
+    this.incidentPriority = data.incidentPriority;
+    this.customerName = data.customerName;
+    this.customerPhoneNumber = data.customerPhoneNumber;
+    this.customerAddress = data.customerAddress;
+    this.incidentNarrative = data.incidentNarrative;
+    this.currentIncidentID = data._id;
+    }
 
 //funtion being called when the user click the button: createIncidentTicket
-createIncidentTicket()
+UpdateRecords()
 {
   var bodyData = {
   "incidentDescription": this.incidentDescription,
@@ -29,16 +56,56 @@ createIncidentTicket()
   "incidentNarrative": this.incidentNarrative 
 };
 
-this.http.post("http://localhost:8000/user/create",bodyData).subscribe((resultData: any) =>
+this.http.patch("http://localhost:5000/incidents" + "/" + this.currentIncidentID,bodyData).subscribe((resultData: any) =>
 {
   console.log(resultData);
-  alert("Incident registered successfully")
-  this.incidentDescription = '';
-  this.incidentPriority = '';
-  this.customerName = '';
-  this.customerPhoneNumber = '';
-  this.customerAddress = '';
-  this.incidentNarrative = '';
+  alert("Incident Updated Successfully!");
+  this.getAllIncident();
+});
+}
+
+setDelete(data: any) {
+  this.http.delete("http://localhost:5000/incidents" + "/" + data._id).subscribe((resultData: any) =>{
+    console.log(resultData);
+    alert("Incident Deleted Successfully!");
+    this.getAllIncident();
+  });
+}
+
+save()
+{
+  if(this.currentIncidentID == '')
+  {
+    this.register();
+  }
+  else
+  {
+    this.UpdateRecords();
+  }
+}
+
+register()
+{
+  var bodyData = {
+  "incidentDescription": this.incidentDescription,
+  "incidentPriority": this.incidentPriority,
+  "customerName": this.customerName,
+  "customerPhoneNumber": this.customerPhoneNumber,
+  "customerAddress": this.customerAddress, 
+  "incidentNarrative": this.incidentNarrative
+  };
+
+  this.http.post("http://localhost:5000/incidents/create",bodyData).subscribe((resultData: any) =>
+  {
+    console.log(resultData);
+    alert("Incident Registered Successfully!");
+    //this.getAllIncident();
+    this.incidentDescription = '';
+    this.incidentPriority = '';
+    this.customerName = '';
+    this.customerPhoneNumber = '';
+    this.customerAddress = '';
+    this.incidentNarrative = ''
   });
  }
 }
